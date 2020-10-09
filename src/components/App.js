e5bdecadcdabeefae3d97af05a7db11e5dbb78ca
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-10-10 04:25:55 +0530

    updated the files

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
-}
-
-
+//changed
+const App = (props) => {
+  return (
+    <>
+      <div style={{ fontSize: "16px", fontWeight: "bold" }}>
+        Name of the Project: &nbsp;
+        <span style={{ color: "skyblue" }}>{props["data-nsprojectname"]}</span>
+      </div>
+      <br></br>
+      <div style={{ fontSize: "16px", fontWeight: "bold" }}>
+        Description of the Project: &nbsp;
+        <span style={{ color: "skyblue" }}>
+          {props["data-nsprojectdescription"]}
+        </span>
+      </div>
+    </>
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+//chanaged
+ReactDOM.render(
+  <App
+    data-nsprojectname="Pixsta"
+    data-nsprojectdescription="It's a social media website where users can connect each other with their interests.
+      Features:
+      User: Signup, Sign In, View Posts, Update Posts, Add new Posts, Like the Posts, Dislike the Posts, follow the User,
+      UnFollow the User.
+      Role: Developer | Tech Stack: ReactJS, NodeJs, ExpressJs, Mongo DB (MERN-Stack)."
+  />,
+  document.getElementById("root")
+);
